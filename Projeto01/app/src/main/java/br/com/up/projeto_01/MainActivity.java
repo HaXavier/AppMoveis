9df57fde84ed224c9d2c56a40f2e51385a2d0556
aluno <Aluno@DOSO027096.educacional.up>
2022-08-08 11:53:44 -0300

    aula01

---
+++ Projeto01/app/src/main/java/br/com/up/projeto_01/MainActivity.java
@@ -0,0 +1,49 @@
+package br.com.up.projeto_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSelector;
+    private TextView textViewTitle;
+    private ArrayList<String> listPhases = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listPhases.add("Comprar um café para o Henrique");
+        listPhases.add("Chamar o prof para o churras");
+        listPhases.add("Pagar uma pizza pro Henrique no Italy");
+
+        buttonSelector = findViewById(R.id.button_selector);
+        textViewTitle = findViewById(R.id.text_title);
+
+
+
+        View.OnClickListener clickListener  = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int Position = new Random().nextInt(listPhases.size());
+
+                String text = listPhases.get(Position);
+
+                textViewTitle.setText(text);
+            }
+        };
+
+        buttonSelector.setOnClickListener(clickListener);
+
+    }
+}
